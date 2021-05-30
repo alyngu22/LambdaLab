f9f2d993631f9148bb404a97a477bef60b5f592c
alyngu22 <alyngu22@bergen.org>
2021-05-29 23:32:09 -0400

    PART SIX DONE

--- src/Console.java
+++ src/Console.java
@@ -30,7 +30,7 @@ public class Console {
     public static String parse(ArrayList<String> tokens) {
         if (tokens.size() == 1) {
             Variable v = new Variable(tokens.get(0));
-            System.out.print(v);
+            return v.toString();
         }
         else {
             if (tokens.size() >= 2) {
@@ -41,12 +41,39 @@ public class Console {
                 else {
                     ArrayList<String> left = new ArrayList<String>();
                     ArrayList<String> right = new ArrayList<String>();
-                    for (int i = 0; i < tokens.size() - 1; i++) {
-                        left.add(tokens.get(i));
+                    if (tokens.get(0).equals("(")) {
+                        for (int i = 1; i < tokens.indexOf(")"); i++) {
+                            left.add(tokens.get(i));
+                        }
+                        if (tokens.lastIndexOf("(") != 0) {
+                            for (int j = tokens.lastIndexOf("(") -1; j < tokens.size() -1 ; j++) {
+                                right.add(tokens.get(j));
+                            }
+                        }
+                        else {
+                            for (int j = tokens.indexOf(")") + 1; j < tokens.size(); j++) {
+                                right.add(tokens.get(j));
+                            }
+                        }
                     }
+                    else if (tokens.get(tokens.size()-1).equals(")")) {
+                        for (int i = tokens.lastIndexOf("(") + 1; i < tokens.size() -1; i++) {
+                            right.add(tokens.get(i));
+                        }
+                        for (int j = 0; j < tokens.lastIndexOf("("); j++) {
+                            left.add(tokens.get(j));
+                        }
+                    }
+                    else {
+                        for (int i = 0; i < tokens.size() - 1; i++) {
+                            left.add(tokens.get(i));
+                        }
+                        right.add(tokens.get(tokens.size()-1));
+                    }
+
                     Application ret;
                     if (left.size() >= 2) {
-                        ret = new Application(parse(left), tokens.get(tokens.size() - 1));
+                        ret = new Application(parse(left), parse(right));
                     } else {
                         ret = new Application(tokens.get(0), tokens.get(1));
                     }
