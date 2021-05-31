d76afbb393aab143325af30732d65d4e93018485
alyngu22 <alyngu22@bergen.org>
2021-05-30 23:57:23 -0400

    PART TWELVE DONE

--- src/Console.java
+++ src/Console.java
@@ -26,10 +26,10 @@ public class Console {
         }
         System.out.printf("Goodbye!");
     }
-    public static String parse(ArrayList<String> tokens) {
+    public static Expression parse(ArrayList<String> tokens) {
         if (tokens.size() == 1) {
             Variable v = new Variable(tokens.get(0));
-            return v.toString();
+            return v;
         }
         else {
             if (tokens.size() >= 2) {
@@ -44,13 +44,13 @@ public class Console {
                         f = new Function(new Variable(tokens.get(1)), new Variable(ex.get(0)));
                     }
                     else{
-                        f = new Function(new Variable(tokens.get(1)), app(ex));
+                        f = new Function(new Variable(tokens.get(1)), parse(ex));
                     }
-                    return f.toString();
+                    return f;
                 }
                 else {
                     Application a = app(tokens);
-                    return a.toString();
+                    return a;
                 }
             }
 
@@ -62,6 +62,11 @@ public class Console {
     public static Application app(ArrayList<String> tokens){
         ArrayList<String> left = new ArrayList<String>();
         ArrayList<String> right = new ArrayList<String>();
+
+
+
+
+
         if (tokens.get(0).equals("(")) {
             if(tokens.indexOf(")")!=tokens.size()-1) {
                 for (int i = 1; i < tokens.indexOf(")"); i++) {
@@ -71,12 +76,14 @@ public class Console {
                     for (int j = tokens.lastIndexOf("(") + 1; j < tokens.size() - 1; j++) {
                         right.add(tokens.get(j));
                     }
-                } else {
+                }
+                else {
                     for (int j = tokens.indexOf(")") + 1; j < tokens.size(); j++) {
                         right.add(tokens.get(j));
                     }
                 }
             }
+
             else{
                 tokens.remove(0);
                 tokens.remove(tokens.size()-1);
@@ -91,15 +98,25 @@ public class Console {
                 left.add(tokens.get(j));
             }
         }
-        else {
+        else if (tokens.contains("\\")) {
+            for (int i = 0; i < tokens.indexOf("\\"); i++) {
+                left.add(tokens.get(i));
+            }
+            for (int j = tokens.indexOf("\\"); j < tokens.size(); j++) {
+                right.add(tokens.get(j));
+            }
+        }
+
+        else  {
+
             for (int i = 0; i < tokens.size() - 1; i++) {
                 left.add(tokens.get(i));
             }
             right.add(tokens.get(tokens.size()-1));
         }
         Application ret;
-        if (left.size() >= 2) {
-            ret = new Application(parse(left), parse(right));
+        if (left.size() >= 2 || right.size() >= 2) {
+            ret = new Application(parse(left).toString(), parse(right).toString());
         } else {
             ret = new Application(tokens.get(0), tokens.get(1));
         }
